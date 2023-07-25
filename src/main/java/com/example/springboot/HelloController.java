20f39d94f3ad1bd401d86cb243333559e50379e6
Robert McNees <rmcnees@vmware.com>
2023-07-25 11:35:25 -0400

    Simple update to v3

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! v2!";
+		return "Greetings from Spring Boot + Tanzu! v3!";
 	}
 
 }
